8811f19eb94742b3ab2f2fb031a72d9fd5637f8d
Hannah Folkertsma <hannahfolkertsma@gmail.com>
2022-04-20 09:10:43 -0500

    fixed some dependency stuff, got styles.css, created basic wordle component

--- ProjectCheckmate/app.tsx
+++ ProjectCheckmate/app.tsx
@@ -1,4 +1,5 @@
-declare var require: any
+declare var require: any;
+import { Wordle } from "./components/wordle";
 
 var React = require('react');
 var ReactDOM = require('react-dom');
@@ -6,7 +7,7 @@ var ReactDOM = require('react-dom');
 export class Hello extends React.Component {
     render() {
         return (
-            <h1>Welcome to React!!</h1>
+            <Wordle/>
         );
     }
 }
---
+++ ProjectCheckmate/components/wordle.tsx
@@ -0,0 +1,25 @@
+declare var require: any
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+import "../styles.css"
+
+
+export const Wordle = () => {
+    return (
+        <table>
+            <tr>
+                <td className="wordleCell" />
+                <td className="wordleCell"/>
+                <td className="wordleCell"/>
+                <td className="wordleCell"/>
+                <td className="wordleCell"/>
+            </tr>
+            <tr />
+            <tr />
+            <tr />
+            <tr />
+        </table>
+        );
+}
