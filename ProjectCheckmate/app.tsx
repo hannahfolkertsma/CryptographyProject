2c30f4486447ecb0a94a4ef2f238489fc7d992bc
Hannah Folkertsma <hannahfolkertsma@gmail.com>
2022-05-01 10:24:26 -0500

    added router

--- ProjectCheckmate/app.tsx
+++ ProjectCheckmate/app.tsx
@@ -1,15 +1,17 @@
 declare var require: any;
 import { Wordle } from "./components/Wordle";
+import { BrowserRouter, Route } from "react-router-dom";
 
 var React = require('react');
 var ReactDOM = require('react-dom');
 
 const App = () => {
     
-        return (
-            <div>
-                <h1>Hello</h1>
-            </div>
+    return (
+        <BrowserRouter>
+            <Route path="/" element={<Wordle />} />
+            <Route path="/" element={<Wordle />} />
+        </BrowserRouter>
         );
     
 }
