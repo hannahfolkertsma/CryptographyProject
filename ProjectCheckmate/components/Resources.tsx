9a3b102f71a2b5696d960ca09fdf1bf81ae5b03a
Hannah Folkertsma <hannahfolkertsma@gmail.com>
2022-05-12 21:33:58 -0500

    fixed links

--- ProjectCheckmate/components/Resources.tsx
+++ ProjectCheckmate/components/Resources.tsx
@@ -13,11 +13,11 @@ export const Resources = () => {
         <div className="spacer"/>
             <div className="pageHeader">RESOURCES</div>
             <div className="spacer" />
-            <div className="text"><Link to={"https://www.dcode.fr/caesar-cipher"}>Caesar Cipher</Link></div>
-            <div className="text"><Link to={"https://www.dcode.fr/vigenere-cipher"}>Vigenere Cipher</Link></div>
-            <div className="text"><Link to={"https://www.binaryhexconverter.com/ascii-text-to-binary-converter"}>Binary Conversion</Link></div>
-            <div className="text"><Link to={"https://xor.pw/"}>XOR</Link></div>
-            <div className="text"><Link to={"https://www.dcode.fr/shift-cipher"}>Shift Cipher</Link></div>
+            <div className="text"><a target="_blank" href="https://www.dcode.fr/caesar-cipher">Caesar Cipher</a></div>
+            <div className="text"><a target="_blank" href="https://www.dcode.fr/vigenere-cipher">Vigenere Cipher</a></div>
+            <div className="text"><a target="_blank" href="https://www.binaryhexconverter.com/ascii-text-to-binary-converter">Binary Cipher</a></div>
+            <div className="text"><a target="_blank" href="https://xor.pw/">XOR</a></div>
+            <div className="text"><a target="_blank" href="https://www.dcode.fr/shift-cipher">Shift Cipher</a></div>
         </div>
        );
 }
